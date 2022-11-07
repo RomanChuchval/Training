8f21708ec343cec90ea65a635f5a7e5d0abcbce1
Roman Chuchval <rchuchval@gmail.com>
2022-11-07 12:13:25 +0300

    training 01. Comment your code

---
+++ FreeCodeCamp/JS/01.js
@@ -0,0 +1,10 @@
+/* You should create a // style comment that contains at least five letters.
+Passed: You should create a /* style comment that contains at least five letters. */
+
+// This is a line comment
+
+/* This is a
+multi-line comment */
+
+
+
