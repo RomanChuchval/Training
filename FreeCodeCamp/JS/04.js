23260cf930c4c2f075951d42b566a96a836ab232
Roman Chuchval <rchuchval@gmail.com>
2022-11-09 23:25:24 +0300

    use backslashes for string

--- FreeCodeCamp/JS/04.js
+++ FreeCodeCamp/JS/04.js
@@ -149,3 +149,8 @@ let c = 33;
 a /= 12;
 b /= 4;
 c /= 11;
+
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+//I am a "double quoted" string inside "double quotes".
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
