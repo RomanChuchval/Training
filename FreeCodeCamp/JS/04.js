5cbcf8b0e81506d76ac32a842d3f7de8e66927c5
Roman Chuchval <rchuchval@gmail.com>
2022-11-09 22:11:14 +0300

    Understanding Uninitialized Variables

--- FreeCodeCamp/JS/04.js
+++ FreeCodeCamp/JS/04.js
@@ -8,10 +8,25 @@ b = a; */
 // Initializing Variables with the Assignment Operator
 
 let myVar = 0;
-let a = 9;
+let n = 9;
 
 // Declare String Variables
 
 let myFirstName = 'Roman'
 let myLastName = 'Chuchval'
 
+/* 
+a should be defined and have a final value of 6.
+Passed: b should be defined and have a final value of 15.
+Passed: c should not contain undefined and should have a final value of the string I am a String!
+Passed: You should not change code below the specified comment. */
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!"
