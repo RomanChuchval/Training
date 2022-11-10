86abcd3d8e3a1243a1c5aa9e78c8b82d5bceb4a8
Roman Chuchval <rchuchval@gmail.com>
2022-11-10 22:52:45 +0300

    escape sequences in string

--- FreeCodeCamp/JS/04.js
+++ FreeCodeCamp/JS/04.js
@@ -158,3 +158,12 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 //Quoting Strings with Single Quotes
 
 const myString = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Escape Sequences in Strings
+
+const threeStrings = "FirstLine\n\t\\SecondLine\nThirdLine"; 
+// Output:
+// FirstLine
+//    \SecondLine
+// ThirdLine
+
