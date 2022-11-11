83b491ddad0a236ec2cf1a7f3c5cfc5b619e3479
Roman Chuchval <rchuchval@gmail.com>
2022-11-11 12:04:12 +0300

    assignment with a returned value

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -68,3 +68,12 @@ function addFive() {
 }
 addThree();
 addFive();
+
+// Assignment with a Returned Value
+
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+processed = processArg(7);
