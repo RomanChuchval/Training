427dff98e8cae22d5a081d7ca0bbc657500f13ac
Roman Chuchval <rchuchval@gmail.com>
2022-11-11 11:59:54 +0300

    function return undefined

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -56,3 +56,15 @@ const outerWear = 'sweater'
     return outerWear;
 }
 console.log(myOutfit());
+
+// Understanding Undefined Value returned from a Function
+
+let sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+function addFive() {
+    sum += 5;
+}
+addThree();
+addFive();
