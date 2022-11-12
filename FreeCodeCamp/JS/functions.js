7cc107a19bfa7b975ab9dfc181438e634d000953
Roman Chuchval <rchuchval@gmail.com>
2022-11-12 12:59:22 +0300

    work with array. push and shift methods

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -77,3 +77,21 @@ function processArg(num) {
     return (num + 3) / 5;
 }
 processed = processArg(7);
+
+/* Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+Add the number to the end of the array, then remove the first element of the array.
+The nextInLine function should then return the element that was removed. */
+
+function nextInLine(arr, item) {
+    arr.push(item);
+    return arr.shift(item);
+}
+
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log(nextInLine([2], 1))
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
