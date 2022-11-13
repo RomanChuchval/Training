910823ab0fa1de01c1f383f8c6861117a14c2c1b
Roman Chuchval <rchuchval@gmail.com>
2022-11-13 13:27:16 +0300

    compare with || operator

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -220,3 +220,13 @@ function testLogicalAnd(val) {
     return "No";
     }
 testLogicalAnd(10);
+
+// Comparisons with the Logical Or Operator
+
+function testLogicalOr(val) {
+    if (val < 10 || val > 20) {
+        return "Outside";
+    }
+    return "Inside";
+    } 
+testLogicalOr(15);
