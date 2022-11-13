b2b6876f25f61412a2cc95b0b6d32feac39ad653
Roman Chuchval <rchuchval@gmail.com>
2022-11-13 13:23:30 +0300

    compare with && operator

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -210,3 +210,13 @@ function testLessOrEqual(val) {
     return "More Than 24";
 }
 testLessOrEqual(10);
+
+// Comparisons with the Logical And Operator
+
+function testLogicalAnd(val) {
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+    }
+    return "No";
+    }
+testLogicalAnd(10);
