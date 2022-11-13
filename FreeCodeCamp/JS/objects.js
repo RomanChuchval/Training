2b80eba63df213dafe2bd71e8305041c80377aef
Roman Chuchval <rchuchval@gmail.com>
2022-11-13 18:32:52 +0300

    Add New Properties to a JavaScript Object

--- FreeCodeCamp/JS/objects.js
+++ FreeCodeCamp/JS/objects.js
@@ -46,3 +46,13 @@ const myHappyDog = {
     "friends": ["freeCodeCamp Campers"]
 };
 myDog.name = 'Happy Coder'
+
+//Add New Properties to a JavaScript Object
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = 'woof-woof'
