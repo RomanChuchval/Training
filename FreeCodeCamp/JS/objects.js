1bc455840ba58ffd2cf84ac5a1e05f259a30c4ff
Roman Chuchval <rchuchval@gmail.com>
2022-11-13 18:16:56 +0300

    make an object

---
+++ FreeCodeCamp/JS/objects.js
@@ -0,0 +1,9 @@
+// Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+const myDog = {
+    name: 'Nord',
+    legs: 4,
+    tails: 1,
+    friends: ['roma', 'lera']
+};
+
