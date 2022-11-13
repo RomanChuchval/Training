3f7ad3b9b396ec35a18f1b9e7c84ea40386b8393
Roman Chuchval <rchuchval@gmail.com>
2022-11-13 13:30:35 +0300

    if else statements

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -230,3 +230,16 @@ function testLogicalOr(val) {
     return "Inside";
     } 
 testLogicalOr(15);
+
+// Introducing Else Statements
+
+function testElse(val) {
+    let result = "";
+    if (val > 5) {
+        result = "Bigger than 5";
+    } else {
+        result = "5 or Smaller";
+    }
+    return result;
+}
+testElse(4);
