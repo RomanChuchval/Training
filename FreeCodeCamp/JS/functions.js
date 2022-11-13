bd9c16f5f16f8151a493ad4cf2c5b9e0ae942f31
Roman Chuchval <rchuchval@gmail.com>
2022-11-13 13:03:26 +0300

    less than operator

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -184,3 +184,16 @@ function testGreaterOrEqual(val) {
     return "Less than 10";
 }
 testGreaterOrEqual(10);
+
+//Comparison with the Less Than Operator
+
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+        return "Under 25";
+    }
+    if (val < 55) {  // Change this line
+        return "Under 55";
+    }
+    return "55 or Over";
+    }
+testLessThan(10);
