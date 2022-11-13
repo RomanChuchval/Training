62d2943770ab402dd03e4bec84e3911b962a9571
Roman Chuchval <rchuchval@gmail.com>
2022-11-13 12:52:46 +0300

    inequality operator

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -141,3 +141,12 @@ function compareEquality(a, b) {
     return "Not Equal";
     }
 compareEquality(10, "10");
+
+// Comparison with the Inequality Operator
+function testNotEqual(val) {
+    if (val != 99) { 
+        return "Not Equal";
+    }
+    return "Equal";
+}
+testNotEqual(10);
