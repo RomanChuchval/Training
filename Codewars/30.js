45761b6b3d19ad2e7160caa125e03777b417c3cf
Roman Chuchval <rchuchval@gmail.com>
2022-11-14 21:13:20 +0300

    how many lightsabers

---
+++ Codewars/30.js
@@ -0,0 +1,19 @@
+/* Inspired by the development team at Vooza, write the function that
+accepts the name of a programmer, and
+returns the number of lightsabers owned by that person.
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+Note: your function should have a default parameter. */
+
+function howManyLightsabersDoYouOwn(name) {
+    switch(name) {
+        case 'Adam':
+            return 0;
+            break;
+        case 'Zach':
+            return 18;
+            break;
+        default:
+            return 0;
+            break;
+    }
+};
