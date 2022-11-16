8301ea0d74025a0853569afdadb230f7ba1eca4c
Roman Chuchval <rchuchval@gmail.com>
2022-11-16 20:12:28 +0300

    For loop

--- FreeCodeCamp/JS/js-loop.js
+++ FreeCodeCamp/JS/js-loop.js
@@ -6,3 +6,13 @@ while (i >= 0) {
     myArray.push(i);
     i--;
 }
+
+//Iterate with JavaScript For Loops
+
+const myArray2 = [];
+let t = 1;
+    for (t = 1; t < 6; t++) {
+    myArray2.push(t);
+    }
+
+// myArray should equal [1, 2, 3, 4, 5].
