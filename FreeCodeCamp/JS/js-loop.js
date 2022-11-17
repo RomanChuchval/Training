61b7e739054dc0024d692e996887e6335dafadc4
Roman Chuchval <rchuchval@gmail.com>
2022-11-17 22:54:40 +0300

    replace loops using recursion

--- FreeCodeCamp/JS/js-loop.js
+++ FreeCodeCamp/JS/js-loop.js
@@ -68,3 +68,13 @@ do {
     myArrayDo.push(r);
     r++;
 } while (r < 5)
+
+// Replace Loops using Recursion
+
+function sum(arr, n) {
+    if (n <= 0) {
+        return 0;
+    } else {
+        return sum(arr, n - 1) + arr [n - 1]
+    }
+}
