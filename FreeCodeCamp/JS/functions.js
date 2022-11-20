b21916c9c87d44bbb3613be3a4a2cd1b5d6183d1
Roman Chuchval <rchuchval@gmail.com>
2022-11-20 23:06:21 +0300

    parseInt() with a Radix

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -476,3 +476,10 @@ function convertToInteger(str) {
     return parseInt(str)
 }
 convertToInteger("56");
+
+// Use the parseInt Function with a Radix
+
+function convertToInteger(str) {
+    return parseInt(str, 2)
+}
+convertToInteger("10011");
