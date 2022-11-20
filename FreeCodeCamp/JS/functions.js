5acd148f6f4ac3859b26426b05bb0a74b2a2559d
Roman Chuchval <rchuchval@gmail.com>
2022-11-20 23:22:03 +0300

    Multiple Conditional (Ternary) Operators

--- FreeCodeCamp/JS/functions.js
+++ FreeCodeCamp/JS/functions.js
@@ -489,3 +489,12 @@ convertToInteger("10011");
 function checkEqual (a, b) {
     return a == b ? 'Equal' : 'Not Equal'
 }
+
+//Use Multiple Conditional (Ternary) Operators
+
+function checkSign(num) {
+    return (num > 0) ? 'positive'
+        : (num < 0) ? 'negative'
+        : 'zero'
+}
+    checkSign(10);
