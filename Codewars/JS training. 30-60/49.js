4220d0b269132ea6cc8e44d21f097eda0d1bbe81
Roman Chuchval <rchuchval@gmail.com>
2022-11-22 23:18:35 +0300

    return sum of arr numbers

---
+++ Codewars/JS training. 30-60/49.js
@@ -0,0 +1,13 @@
+/* Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0. */
+
+function sum (numbers) {
+    let sum = 0;
+        if ( numbers.length !== 0) {
+            for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i]
+            }
+        } else {
+            return 0
+        }
+    return sum
+}
