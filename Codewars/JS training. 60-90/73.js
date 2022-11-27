96f4fd65a11782f7932b120647022106a666c7c3
Roman Chuchval <rchuchval@gmail.com>
2022-11-27 17:47:09 +0300

    name === owners ? hello boss

---
+++ Codewars/JS training. 60-90/73.js
@@ -0,0 +1,9 @@
+/* Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+name equals owner	'Hello boss'
+otherwise	'Hello guest' */
+
+greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest'
+
