b837c22185cd3d45358e32ff6261470243307c34
Roman Chuchval <rchuchval@gmail.com>
2022-11-28 21:38:02 +0300

    Destructuring Assignment to Assign Variables from Objects

--- FreeCodeCamp/JS/ES6/const.js
+++ FreeCodeCamp/JS/ES6/const.js
@@ -11,3 +11,13 @@ s[2] = 7
   // Only change code above this line
 }
 editInPlace();
+
+// Use Destructuring Assignment to Assign Variables from Objects
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES
