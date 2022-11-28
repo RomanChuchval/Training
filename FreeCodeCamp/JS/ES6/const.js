c3c5c749814ec2f2d203c34f8d00085549bf8630
Roman Chuchval <rchuchval@gmail.com>
2022-11-28 22:33:38 +0300

    Destructuring Assignment to Pass an Object as a Function's Parameters

--- FreeCodeCamp/JS/ES6/const.js
+++ FreeCodeCamp/JS/ES6/const.js
@@ -47,3 +47,18 @@ function removeFirstTwo(list) {
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source);
 // return [3, 4, 5, 6, 7, 8, 9, 10]
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+
+const stats = {
+  max: 56.078,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = ({max, min}) => (max + min) / 2.0;
+console.log(half(stats));
+// (stats.max + stats.min) / 2 = 27.664
