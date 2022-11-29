1d46f828b8c4b4bbf373db7ab74e131fa03faaff
Roman Chuchval <rchuchval@gmail.com>
2022-11-29 18:51:15 +0300

    add output

--- FreeCodeCamp/JS/ES6/const.js
+++ FreeCodeCamp/JS/ES6/const.js
@@ -129,3 +129,7 @@ class Thermostat {
     this.temp = updateTemp
   }
 }
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
