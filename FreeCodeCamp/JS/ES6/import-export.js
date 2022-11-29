82483d6fd1292f0163d5caf6b40b8b37ba66551b
Roman Chuchval <rchuchval@gmail.com>
2022-11-29 20:13:14 +0300

    Reuse JavaScript Code Using import

--- FreeCodeCamp/JS/ES6/import-export.js
+++ FreeCodeCamp/JS/ES6/import-export.js
@@ -9,3 +9,12 @@ const lowercaseString = (string) => {
 };
 
 export { uppercaseString, lowercaseString };
+
+
+//Reuse JavaScript Code Using import
+
+import {uppercaseString, lowercaseString} from './string_functions.js'
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
