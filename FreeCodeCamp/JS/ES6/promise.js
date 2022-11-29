fc1237e6c1c812a0e4c72429d92a53cac159ef7b
Roman Chuchval <rchuchval@gmail.com>
2022-11-29 20:43:29 +0300

    Handle a Fulfilled Promise with then

--- FreeCodeCamp/JS/ES6/promise.js
+++ FreeCodeCamp/JS/ES6/promise.js
@@ -4,7 +4,6 @@ const makeServerRequest = new Promise((resolve, reject) => {});
 
 // Complete a Promise with resolve and reject
 
-
 const makeRequest = new Promise((resolve, reject) => {
     // responseFromServer represents a response from a server
     let responseFromServer;
@@ -14,4 +13,21 @@ const makeRequest = new Promise((resolve, reject) => {
     } else {
         reject("Data not received");
     }
+});
+
+// Handle a Fulfilled Promise with then
+
+const ServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+ServerRequest.then((result) => {
+    console.log(result); // We got the data
 });
