c9c777e40b91f3400d6dc8656a4ae6be8c5dce4b
Roman Chuchval <rchuchval@gmail.com>
2022-12-01 22:49:28 +0300

    Match Ending String Patterns

--- FreeCodeCamp/JS/Regex/regex.js
+++ FreeCodeCamp/JS/Regex/regex.js
@@ -87,3 +87,9 @@ let result = text.match(myRegex);
 let rickyAndCal = "Cal and Ricky both like racing.";
 let calRegex = /^Cal/; // Change this line
 let result = calRegex.test(rickyAndCal);
+
+// Match Ending String Patterns
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
