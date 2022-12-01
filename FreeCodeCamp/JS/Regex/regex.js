865ef6137b6d04383232267780239c8a4b355ef3
Roman Chuchval <rchuchval@gmail.com>
2022-12-01 23:10:25 +0300

    Match All Numbers

--- FreeCodeCamp/JS/Regex/regex.js
+++ FreeCodeCamp/JS/Regex/regex.js
@@ -105,3 +105,9 @@ let result = quoteSample.match(alphabetRegexV2).length;
 let quoteSample = "The five boxing wizards jump quickly.";
 let nonAlphabetRegex = /\W/g; // Change this line
 let result = quoteSample.match(nonAlphabetRegex).length;
+
+// Match All Numbers
+
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
