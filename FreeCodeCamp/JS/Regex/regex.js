7ec509ed0e4c67f72226ef53b34b267b58879c27
Roman Chuchval <rchuchval@gmail.com>
2022-12-01 22:43:14 +0300

    Find Characters with Lazy Matching

--- FreeCodeCamp/JS/Regex/regex.js
+++ FreeCodeCamp/JS/Regex/regex.js
@@ -75,3 +75,9 @@ let result = difficultSpelling.match(myRegex);
 
 let chewieRegex = /Aa*/; // Change this line
 let result = chewieQuote.match(chewieRegex);
+
+// Find Characters with Lazy Matching
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
