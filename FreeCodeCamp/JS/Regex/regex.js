ff5f81a95bc8789462e9a37044aff6b4e8e22055
Roman Chuchval <rchuchval@gmail.com>
2022-12-01 22:19:46 +0300

    Match Numbers and Letters of the Alphabet

--- FreeCodeCamp/JS/Regex/regex.js
+++ FreeCodeCamp/JS/Regex/regex.js
@@ -8,47 +8,53 @@ let result = myRegex.test(myString); // output > true
 
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
-let result2 = waldoRegex.test(waldoIsHiding);
+let result = waldoRegex.test(waldoIsHiding);
 
 // Match a Literal String with Different Possibilities
 
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
-let result3 = petRegex.test(petString);
+let result = petRegex.test(petString);
 
 // Ignore Case While Matching
 
 let myString2 = "freeCodeCamp";
 let fccRegex = /freecodecamp/i; // Change this line
-let result4 = fccRegex.test(myString);
+let result = fccRegex.test(myString);
 
 // Extract Matches
 
 
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
-let result5 = extractStr.match(codingRegex) // Change this line
+let result = extractStr.match(codingRegex) // Change this line
 
 //
 
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /twinkle/gi; // Change this line
-let result6 = twinkleStar.match(starRegex); // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
 
 // Match Anything with Wildcard Period
 
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
-let result7 = unRegex.test(exampleStr);
+let result = unRegex.test(exampleStr);
 
 // Match Single Character with Multiple Possibilities
 
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
-let result8 = quoteSample.match(vowelRegex); // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
 
 // Match Letters of the Alphabet
 
 let quoteSampleStr = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/ig; // Change this line
-let result9 = quoteSample.match(alphabetRegex); // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+// Match Numbers and Letters of the Alphabet
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
