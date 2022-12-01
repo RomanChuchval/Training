5f5be500cced22c99a13a4e0632ea3bd4716dcc3
Roman Chuchval <rchuchval@gmail.com>
2022-12-01 21:29:54 +0300

    Match Literal Strings

--- FreeCodeCamp/JS/Regex/regex.js
+++ FreeCodeCamp/JS/Regex/regex.js
@@ -3,3 +3,9 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // output > true
+
+// Match Literal Strings
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result2 = waldoRegex.test(waldoIsHiding);
