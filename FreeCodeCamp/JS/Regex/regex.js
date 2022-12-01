941995f6dd5cabb271658747a28be5b6776a2b25
Roman Chuchval <rchuchval@gmail.com>
2022-12-01 23:01:07 +0300

    Match All Letters and Numbers

--- FreeCodeCamp/JS/Regex/regex.js
+++ FreeCodeCamp/JS/Regex/regex.js
@@ -93,3 +93,9 @@ let result = calRegex.test(rickyAndCal);
 let caboose = "The last car on a train is the caboose";
 let lastRegex = /caboose$/; // Change this line
 let result = lastRegex.test(caboose);
+
+// Match All Letters and Numbers
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
