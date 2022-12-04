44ffcdf59b372540e035db874c9061822978e536
Roman Chuchval <rchuchval@gmail.com>
2022-12-04 16:02:21 +0300

    Sum The Strings

---
+++ Codewars/JS training. 90 - 120/98.js
@@ -0,0 +1,22 @@
+/* Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2" */
+
+function sumStr(a, b) {
+    if (a == "" && b == "") {
+        return "0";
+    } else if (a == "" || b == "") {
+        if (a == "") {
+            return b;
+        }
+        return a;
+    } else {
+        return (a * 1 + b * 1).toString();
+    }
+}
