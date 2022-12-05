ad27254ee3c582e730eed07535fd5464b7c321fe
Roman Chuchval <rchuchval@gmail.com>
2022-12-05 21:18:01 +0300

    typeof

--- FreeCodeCamp/JS/debugging.js
+++ FreeCodeCamp/JS/debugging.js
@@ -14,3 +14,12 @@ console.log(sumAB);
 let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 console.log(output)
 console.clear()
+
+// typeof
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven)
+console.log(typeof three)
