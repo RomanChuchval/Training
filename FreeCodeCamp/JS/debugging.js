cdc026a1b3cebc14029c96f2e09c0a1c7348386f
Roman Chuchval <rchuchval@gmail.com>
2022-12-05 21:22:42 +0300

    Catch Misspelled Variable and Function Names

--- FreeCodeCamp/JS/debugging.js
+++ FreeCodeCamp/JS/debugging.js
@@ -23,3 +23,10 @@ console.log(seven + three);
 // Only change code below this line
 console.log(typeof seven)
 console.log(typeof three)
+
+// Catch Misspelled Variable and Function Names
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
