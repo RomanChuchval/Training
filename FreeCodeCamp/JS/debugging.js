9cb2e3a96f4eda3d62a64fb6a9df17e8ebe0e3c5
Roman Chuchval <rchuchval@gmail.com>
2022-12-05 21:35:49 +0300

    Catch Off By One Errors When Using Indexing

--- FreeCodeCamp/JS/debugging.js
+++ FreeCodeCamp/JS/debugging.js
@@ -56,3 +56,17 @@ if (x == y) {
 }
 
 console.log(result);
+
+// Catch Off By One Errors When Using Indexing
+
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i <= len - 1; i++) {
+        // Only change code above this line
+        console.log(firstFive[i]);
+    }
+}
+
+countToFive();
