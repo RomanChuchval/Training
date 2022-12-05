bf6bde386badd833c6b0df16dde7d10c7d44933b
Roman Chuchval <rchuchval@gmail.com>
2022-12-05 21:31:47 +0300

    Catch Use of Assignment Operator Instead of Equality Operator

--- FreeCodeCamp/JS/debugging.js
+++ FreeCodeCamp/JS/debugging.js
@@ -5,15 +5,16 @@ let b = 1;
 a++;
 // Only change code below this line
 
-console.log(a)
+console.log(a);
 let sumAB = a + b;
 console.log(sumAB);
 
 // Understanding the Differences between the freeCodeCamp and Browser Console
 
-let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
-console.log(output)
-console.clear()
+let output =
+    "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
 
 // typeof
 
@@ -21,8 +22,8 @@ let seven = 7;
 let three = "3";
 console.log(seven + three);
 // Only change code below this line
-console.log(typeof seven)
-console.log(typeof three)
+console.log(typeof seven);
+console.log(typeof three);
 
 // Catch Misspelled Variable and Function Names
 
@@ -34,10 +35,24 @@ console.log(`Net working capital is: ${netWorkingCapital}`);
 // Catch Unclosed Parentheses, Brackets, Braces and Quotes
 
 let myArray = [1, 2, 3];
-let arraySum = myArray.reduce((previous, current) =>  previous + current);
+let arraySum = myArray.reduce((previous, current) => previous + current);
 console.log(`Sum of array values is: ${arraySum}`);
 
 // Catch Mixed Usage of Single and Double Quotes
 
 let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
 console.log(innerHtml);
+
+// Catch Use of Assignment Operator Instead of Equality Operator
+
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if (x == y) {
+    result = "Equal!";
+} else {
+    result = "Not equal!";
+}
+
+console.log(result);
