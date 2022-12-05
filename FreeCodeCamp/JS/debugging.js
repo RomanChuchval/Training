9682010a35d5f549f53a7ce358cf6fb6378ae422
Roman Chuchval <rchuchval@gmail.com>
2022-12-05 21:59:10 +0300

    Use Caution When Reinitializing Variables Inside a Loop

--- FreeCodeCamp/JS/debugging.js
+++ FreeCodeCamp/JS/debugging.js
@@ -70,3 +70,21 @@ function countToFive() {
 }
 
 countToFive();
+
+//
+
+function zeroArray(m, n) {
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+        row = [];
+        for (let j = 0; j < n; j++) {
+            row.push(0);
+        }
+        newArray.push(row);
+    }
+    return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
