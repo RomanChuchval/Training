ab654e816b5ecc6ef33845527a08ae5139ea64ee
Roman Chuchval <rchuchval@gmail.com>
2022-12-07 20:12:00 +0300

    Copy Array Items Using slice()

--- FreeCodeCamp/JS/data-structures.js
+++ FreeCodeCamp/JS/data-structures.js
@@ -57,3 +57,16 @@ console.log(
         "FireBrick",
     ])
 );
+
+// Copy Array Items Using slice()
+
+function forecast(arr) {
+    // Only change code below this line
+
+    return arr.slice(2, 4);
+}
+
+// Only change code above this line
+console.log(
+    forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
+);
