b91d4cd0cf2e36f37c4c9455fd66647eed2f4b45
Roman Chuchval <rchuchval@gmail.com>
2022-12-07 19:17:48 +0300

    Access an Array's Contents Using Bracket Notation

--- FreeCodeCamp/JS/data-structures.js
+++ FreeCodeCamp/JS/data-structures.js
@@ -1,3 +1,9 @@
 // Use an Array to Store a Collection of Data
 
 let yourArray = ['string', true, 3, false, 55];
+
+// Access an Array's Contents Using Bracket Notation
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = 'R'
+console.log(myArray);
