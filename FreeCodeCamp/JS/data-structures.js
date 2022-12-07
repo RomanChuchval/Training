2fdfd55c17efc19b336259bf73838332c2438cd0
Roman Chuchval <rchuchval@gmail.com>
2022-12-07 19:33:17 +0300

    Add Items to an Array with push() and unshift()

--- FreeCodeCamp/JS/data-structures.js
+++ FreeCodeCamp/JS/data-structures.js
@@ -1,9 +1,21 @@
 // Use an Array to Store a Collection of Data
 
-let yourArray = ['string', true, 3, false, 55];
+let yourArray = ["string", true, 3, false, 55];
 
 // Access an Array's Contents Using Bracket Notation
 
 let myArray = ["a", "b", "c", "d"];
-myArray[1] = 'R'
+myArray[1] = "R";
 console.log(myArray);
+
+// Add Items to an Array with push() and unshift()
+
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift("I", 2, "three");
+    arr.push(7, "VIII", 9);
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(["IV", 5, "six"]));
